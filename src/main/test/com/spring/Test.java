361be4110f2ca65f49d11be9f694ff78df267a3d
zhaizhiyang <zhaizhiyanghao@163.com>
2018-08-03 00:08:58 +0800

    2018-08-03

--- src/main/test/com/spring/Test.java
+++ src/main/test/com/spring/Test.java
@@ -1,13 +1,16 @@
 package com.spring;
 
 import com.springmvc.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static org.mockito.Mockito.*;
 public class Test {
 
    @org.junit.Test
     public void testMock() {
-       UserDao user = mock(UserDao.class);
-       user.findByUsernameAndPwd("admin","123");
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-mvc.xml");
+       UserDao userDao = mock(UserDao.class);
+       verify(userDao, times(1)).findByUsernameAndPwd("admin","123");
     }
 }
